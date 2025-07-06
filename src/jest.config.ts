6674455f2a127169a8cf793ed3e4475722ae3cd4
JesusRonquillo <bionelectronic@gmail.com>
2025-07-06 15:34:41 -0500

    chore: remove deprecated files and complete refactoring

--- src/jest.config.ts
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-  transform: {
-    "^.+\\.tsx?$": "babel-jest",
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-};
