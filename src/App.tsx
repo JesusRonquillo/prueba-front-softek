8d4971278a61d956a7e7b0091e33a4f9e14372b1
JesusRonquillo <bionelectronic@gmail.com>
2025-07-08 01:57:03 -0500

    � cleanup: Eliminación de archivos obsoletos y refactorización

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import Oferts from "./pages/Oferts";
 import Summary from "./pages/Summary";
+import WorkInProgress from "./pages/WorkInProgress";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/oferts-user" element={<Oferts />} />
-        <Route path="/oferts-user/summary-user" element={<Summary />} />
-      </Routes>
+    <div className="app-layout">
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/oferts-user" element={<Oferts />} />
+          <Route path="/oferts-user/summary-user" element={<Summary />} />
+          <Route path="/work-in-progress" element={<WorkInProgress />} />
+        </Routes>
+      </main>
     </div>
   );
 };
